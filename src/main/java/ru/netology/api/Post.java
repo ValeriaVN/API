f1206a15f3d1d010db91628bf692e127d7655cd0
Valeria N <poponuk@gmail.com>
2022-03-27 22:58:38 +0300

    api

---
+++ src/main/java/ru/netology/api/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.api;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+    public FormDate birthday;
+
+}
